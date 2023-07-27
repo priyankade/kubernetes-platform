3cf734e6f3c7e3b37760af19cedc9b4315b939ee
Priyanka De <p.priyankade@gmail.com>
2023-07-27 10:51:19 -0400

    cert add in kube provider

--- src/provider.tf
+++ src/provider.tf
@@ -22,6 +22,7 @@ data "aws_eks_cluster_auth" "cluster" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   #cluster_ca_certificate = base64decode(module.eks.eks_cluster_name.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   # load_config_file       = false
